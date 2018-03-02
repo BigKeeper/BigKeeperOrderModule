f9cf1954f0b3035dd90a945336110433c9281c14
Serena_R <lirui050716@163.com>
2018-03-02 18:21:02 +0800

    Initial commit

---
+++ Example/BigKeeperOrderModule/BigKeeperAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BigKeeperAppDelegate.h
+//  BigKeeperOrderModule
+//
+//  Created by serena1993 on 03/02/2018.
+//  Copyright (c) 2018 serena1993. All rights reserved.
+//
+
+@import UIKit;
+
+@interface BigKeeperAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
